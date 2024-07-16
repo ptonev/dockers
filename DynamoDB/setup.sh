89587ebc66cbf0566faad34f39fa1fce762fa739
Petio Tonev <petio.tonev@ncb.global>
2024-07-16 18:17:01 +0300

    Added DynamoDB & Zipkin

---
+++ DynamoDB/setup.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+echo -e "\nSetup DynamoDB Local & AdminUI docker environment...\n"
+
+#   Get container name
+read -p "Enter DynamoDB container name (or press enter to set 'dynamodb-local' as default): " containerName
+
+#   Check for container name
+if [[ -z "$containerName" ]]; then
+    containerName="dynamodb-local"
+fi
+
+#   Set file name
+dockerComposeFile=./docker-compose.yml
+
+#   Check file exists
+if [[ -e $dockerComposeFile ]]; then
+    #   Remove file
+    rm $dockerComposeFile
+fi
+
+#   Create init-mongo.js
+cat > $dockerComposeFile <<EOF
+version: '3.7'
+services:
+  dynamodb-local:
+    image: amazon/dynamodb-local:latest
+    container_name: '$containerName'
+    ports:
+      - "8000:8000"
+
+  dynamodb-admin:
+    image: aaronshaf/dynamodb-admin
+    ports:
+      - "8001:8001"
+    environment:
+      DYNAMO_ENDPOINT: "http://dynamodb-local:8000"
+      AWS_REGION: "us-west-2"
+      AWS_ACCESS_KEY_ID: local
+      AWS_SECRET_ACCESS_KEY: local
+    depends_on:
+      - dynamodb-local
+EOF
+
+#   Compose up.sh file name
+fileName=./up.sh
+#   Create up.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose up -d
+EOF
+chmod a+x $fileName
+
+#   Compose down.sh file name
+fileName=./down.sh
+#   Create down.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose down
+EOF
+chmod a+x $fileName
+
+#   Compose start.sh file name
+fileName=./start.sh
+#   Create start.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose start
+EOF
+chmod a+x $fileName
+
+#   Compose start.sh file name
+fileName=./stop.sh
+#   Create stop.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose stop
+EOF
+chmod a+x $fileName
+
+#   Compose login.sh file name
+fileName=./login.sh
+#   Create login.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker exec -it $containerName sh
+EOF
+chmod a+x $fileName
+
+#   Compose logs.sh file name
+fileName=./logs.sh
+#   Create logs.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker container logs $containerName
+EOF
+chmod a+x $fileName
+
+#   Print help information
+cat << EOF
+
+Setup done.
+
+To up container type: 'docker-compose up -d' or './up.sh'
+To down container type: 'docker-compose down' or './down.sh'
+To start container type: 'docker-compose start' or './start.sh'
+To stop container type: 'docker-compose stop' or './stop.sh'
+To log in (exec) into container type: 'docker exec -it $containerName bash' or './login.sh'
+To view logs for container type: 'docker container logs $containerName' or './logs.sh'
+
+EOF
---
+++ Zipkin/setup.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+echo -e "\nSetup Zipkin docker environment...\n"
+
+#   Get image tag
+read -p "Enter image tag (or press enter to set 'latest' as default): " imageTag
+
+#   Check for image tag
+if [[ -z "$imageTag" ]]; then
+    image="openzipkin/zipkin:latest"
+else
+    image="openzipkin/zipkin:$imageTag"
+fi
+
+#   Get image
+hasImage=$(docker images -q $image)
+
+#   Check for image
+if [[ -z "$hasImage" ]]; then
+    echo
+    #   Pull image
+    docker pull $image
+    echo
+fi
+
+#   Get container name
+read -p "Enter container name (or press enter to set 'zipkin' as default): " containerName
+
+#   Check for container name
+if [[ -z "$containerName" ]]; then
+    containerName="zipkin"
+fi
+
+#   Set file name
+dockerComposeFile=./docker-compose.yml
+
+#   Check file exists
+if [[ -e $dockerComposeFile ]]; then
+    #   Remove file
+    rm $dockerComposeFile
+fi
+
+#   Create init-mongo.js
+cat > $dockerComposeFile <<EOF
+version: "3"
+services:
+  zipkin:
+    image: '$image'
+    container_name: '$containerName'
+    restart: on-failure:10
+    environment:
+      - STORAGE_TYPE=mem
+      # Uncomment to enable self-tracing
+      # - SELF_TRACING_ENABLED=true
+      # Uncomment to increase heap size
+      # - JAVA_OPTS=-Xms128m -Xmx128m -XX:+ExitOnOutOfMemoryError
+    ports:
+      # Port used for the Zipkin UI and HTTP Api
+      - 9411:9411
+    network_mode: 'host'
+EOF
+
+#   Compose up.sh file name
+fileName=./up.sh
+#   Create up.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose up -d
+EOF
+chmod a+x $fileName
+
+#   Compose down.sh file name
+fileName=./down.sh
+#   Create down.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose down
+EOF
+chmod a+x $fileName
+
+#   Compose start.sh file name
+fileName=./start.sh
+#   Create start.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose start
+EOF
+chmod a+x $fileName
+
+#   Compose start.sh file name
+fileName=./stop.sh
+#   Create stop.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker-compose stop
+EOF
+chmod a+x $fileName
+
+#   Compose login.sh file name
+fileName=./login.sh
+#   Create login.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker exec -it $containerName bash
+EOF
+chmod a+x $fileName
+
+#   Compose logs.sh file name
+fileName=./logs.sh
+#   Create logs.sh
+cat > $fileName <<EOF
+#!/bin/bash
+docker container logs $containerName
+EOF
+chmod a+x $fileName
+
+#   Print help information
+cat << EOF
+
+Setup done.
+
+To up container type: 'docker-compose up -d' or './up.sh'
+To down container type: 'docker-compose down' or './down.sh'
+To start container type: 'docker-compose start' or './start.sh'
+To stop container type: 'docker-compose stop' or './stop.sh'
+To log in (exec) into container type: 'docker exec -it $containerName bash' or './login.sh'
+To view logs for container type: 'docker container logs $containerName' or './logs.sh'
+
+EOF
